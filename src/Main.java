3bd2698384fbc7e21bfeaad06cd354441fe91b5e
Tinkov Maxim <vasy499@mail.ru>
2022-06-05 23:00:28 +0300

    Мой первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        out.println("Hello world!");
     }
 }
